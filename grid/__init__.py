da19fc194932b3946a397a78d4bcaf71271903cd
Yaguang Li <41059765+parallelpro@users.noreply.github.com>
2026-08-11 11:27:52 -1000

    Untrack files matched by .gitignore
    Removes demo.ipynb, grid/, and test_samples/*.csv|xlsx from tracking;
    files remain on disk locally.

--- grid/__init__.py
+++
@@ -1,4 +0,0 @@
-from .src import grid, get_surface_correction, get_model_Dnu, get_obs_Dnu, match_modes
-from .params import user_setup_params
-
-__all__ = ['get_surface_correction', 'grid', 'get_model_Dnu', 'get_obs_Dnu', 'match_modes', 'user_setup_params']
